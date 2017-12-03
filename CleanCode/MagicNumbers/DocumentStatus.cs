2d43aa524b81fd77257f970a9d87c0d6fdcb4a46
lekhanhtrinhbp <lekhanhtrinhbp@hotmail.com>
2017-12-03 11:01:26 +0700

    fix magic numbers

--- CleanCode/MagicNumbers/DocumentStatus.cs
+++ CleanCode/MagicNumbers/DocumentStatus.cs
@@ -1,8 +1,15 @@
-﻿namespace CleanCode.MagicNumbers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.MagicNumbers
 {
     public enum DocumentStatus
     {
-        Draft = 1,
-        Lodged = 2
+        Draft =1,
+        Lodged = 2,
+
     }
 }
